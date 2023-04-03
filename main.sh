409095cca1eb1251410f2dd0695e7dd8c28cee96
0x6775737461 <fabiogustavo@tuta.io>
2023-04-02 21:59:04 -0400

    visualizando o perfil de usuário no UFGDNET

--- main.sh
+++ main.sh
@@ -1,9 +1,14 @@
+#!/bin/bash
+
 # No site da UFGD esse domínio é usado só pra ser redirecionado
-main_link='sigecad-academico.ufgd.edu.br'
+second_link='https://sigecad-academico.ufgd.edu.br'
+
+main_link='https://login.ufgd.edu.br/login_form'
+third_link='https://ufgdnet.ufgd.edu.br/perfil/'
 
 # "Response Header"
 response=$(
-   curl -s --insecure -iL  "${main_link}" -w '%{header_json}' \
+   curl -s --insecure -iL  "${second_link}" -w '%{header_json}' \
       -o /tmp/response.json | jq -r
 )
 
@@ -13,4 +18,44 @@ set_cookie=$(jq -r '.["set-cookie"]' <<< "$response")
 play_session=$(grep -Po '(?<=PLAY_SESSION=)[a-z0-9]{40}' <<< "$set_cookie")
 at=$(grep -Po '(?<=AT=)[a-z0-9]{40}' <<< "$set_cookie")
 
-cookie=""
+cookie="PLAY_SESSION=${play_session}-___AT=${at}"
+
+# Esse User-Agent é aleatório, mude pra outro se quiser
+user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
+AppleWebKit/537.36 (KHTML, like Gecko) \
+Chrome/42.0.2311.135 Safari/537.36 \
+Edge/12.246"
+
+# O arquivo credentials.json possui as informações de login,
+# mude os dados de exemplo dele pra as suas credenciais.
+username=$(jq -r '.username' ./credentials.json)
+password=$(jq -r '.password' ./credentials.json)
+
+# Dados que serão utilizados no POST
+data="authenticityToken=${play_session}&\
+user.username=${username}&\
+user.password=${password}"
+
+post_response=$(curl -sS -X POST \
+   -w '%{header_json}' \
+   --data-raw "$data" \
+   --insecure -i "$main_link" \
+   --user-agent "$user_agent" \
+   --cookie "${cookie}"
+)
+
+# Pegando o token UFGDNET e atribuindo as informações de usuário
+# ao fim.
+ufgd_net=$(grep -Po '(?<=UFGDNET=)[a-z0-9]{40}' <<< "$post_response")
+ufgd_net="${ufgd_net}-${username}"
+
+curl -sS --insecure --user-agent "$user_agent" \
+   --cookie "UFGDNET=${ufgd_net}" \
+   -iL "$third_link" \
+   -o output.zip
+
+# Removendo informações de cabeçalho da requisição GET
+sed -i '1,/^[[:space:]]*$/d' output.zip
+
+# Visualizando os dados da saída que esta em formato gzip
+zcat output.zip
